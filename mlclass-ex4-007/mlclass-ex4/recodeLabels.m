1ffbbcc82a310a84b9931faa552a7ffa2359eae2
Byron Llerena <byron.llerena.z@gmail.com>
2014-11-29 13:49:16 -0500

    Function to recode labels for the output layer in a neural network for back prop

---
+++ mlclass-ex4-007/mlclass-ex4/recodeLabels.m
@@ -0,0 +1,19 @@
+function[recodedMatrix]=recodeLabels(y,numberOfLabels)
+
+%This function takes a vector of dimensions m x 1 and turns it into a matrix of
+%dimensions 'numberOfLabels x m' where each value of the vector is a column
+%of the new matrix e.g(for a vector where the num of labels are 10  8 = 0000000100)
+
+m=size(y,1);
+
+recodedMatrix=zeros(numberOfLabels,m);
+
+%for i=1:m
+
+%recodedMatrix(y(i),i)=1;
+
+
+%end
+
+
+end
