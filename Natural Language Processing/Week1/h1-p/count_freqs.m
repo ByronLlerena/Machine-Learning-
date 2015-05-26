1d279f177f04dcfd1a2cb9c6a2dc5f331c9759fc
ByronLlerena <byron.llerena.z@gmail.com>
2015-05-25 19:51:36 -0500

    Ya cuenta todas las palabras, las clasifica bien, guarda los espacios blancos en una matriz aparte

--- Natural Language Processing/Week1/h1-p/count_freqs.m
+++ Natural Language Processing/Week1/h1-p/count_freqs.m
@@ -1,66 +1,64 @@
 
 function[countsWords, words,typesWords,counts1Gram,types1Gram,counts2Gram,types2Gram,counts3Gram,type3Gram]=count_freqs(data);
 
-
+blankspaces=0;
 countsWords=0;
 words={};
-typesWords={}
+typesWords={};
 counts1Gram=0;
 types1Gram={};
 counts2Gram=0;
 types2Gram={};
 counts3Gram=0;
 types3Gram={};
-line1=0
-%line2=0;
+line1=0;
 arreglo1=0;
-%arreglo2=0;
 aux1="**";
 aux0="*";
 fseek(data,0,-1);
 compare1=0;
-%compare2=0;
-n=1;
 
+n=1;
+contador=1;
 
 
 %I take the forst two lines
 
-line1=fgets(data)
-%line2=fgetl(data);
-
-while(line1!=-1)% || line2!=-1)
-%I verify first if its the end of  sentence
-
- %   if(line1!=-1&&line2!=-1)%Both lines are in the same sentence
-    
-
-        arreglo1=strsplit(line1)
-        compare1=strcmp(arreglo1(1),words)
-[n,words,typesWords,countsWords]=countWords(n,compare1,words,typesWords,countsWords,arreglo1);        
-        
-       % arreglo2=strsplit(line2);
-       % compare2=strcmp(arreglo2(1),words);
-%[n,words,typesWords,countsWords]=countWords(n,compare2,words,typesWords,countsWords,arreglo2);        
-
- %   elseif(line1==-1&&line2!=-1)%There is a new sentence
-        
-  %      arreglo2=strsplit(line2);
-   %     compare2=strcmp(arreglo2(1),words);
-%[n,words,typesWords,countsWords]=countWords(n,compare2,words,typesWords,countsWords,arreglo2);        
-    
- %   elseif(line1!=-1&&line2==-1)%Thats the end of a sentence
-   
-  %      arreglo1=strsplit(line1);
-  %      compare1=strcmp(arreglo1(1),words);
-%[n,words,typesWords,countsWords]=countWords(n,compare1,words,typesWords,countsWords,arreglo1);        
-%endif 
+line1=fgetl(data)
 
 
+while(line1!=-1)
 
-% I take two lines because every each sentence there is a blank space and that would yield -1
-line1=fgets(data);
-%line2=fgetl(data);
+
+          arreglo1=strsplit(line1," ");
+          compare1=strcmp(arreglo1(1),words);
+
+          d=0;
+
+          if(any(compare1)) %Ya existe dicha palabra en el grupo
+              d=find(compare1);%finds the index where the word is stored
+              countsWords(d)=countsWords(d)+1;
+
+           else
+
+          words(n)=arreglo1(1);
+          typesWords(n)=arreglo1(2);
+          countsWords(n,1)=1;
+          n++;
+
+          endif
+
+	  line1=fgetl(data)
+
+
+	    if(strcmp(line1,""))
+            printf("\n Espacio vacio en %d\n\n",contador);
+	    line1=fgetl(data)
+	    blankspaces(end+1)=contador;
+            endif
+
+	    contador++;
+
 
 endwhile
 
